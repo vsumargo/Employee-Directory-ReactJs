12b195842a203aa9c3808d4dc0ea4111a8ee674b
Vsumargo <vincent.sumargo@gmail.com>
2021-04-12 23:48:47 +0800

    edit package.json

--- src/components/EmpoyeesCardContainer.js
+++ src/components/EmpoyeesCardContainer.js
@@ -5,7 +5,7 @@ function EmployeesCardContainer (props) {
     if (props.employees === null){
         return
     }
-    
+
     function populateCards () {
         let employeesCard = props.employees.map( ({gender, name: {first,last}, email, dob:{age}, picture},index) => {
             return (
@@ -30,7 +30,7 @@ function EmployeesCardContainer (props) {
             value={props.sortvalue} 
             handleSortChange={props.handleSortChange}   
             />
-            { props.employees.length === 0 ? <div>No employee found matching the searched name</div> : populateCards()}
+            { props.employees.length === 0 ? <div className="col-12">No employee found matching the searched name</div> : populateCards()}
         </div>
     )
 }
